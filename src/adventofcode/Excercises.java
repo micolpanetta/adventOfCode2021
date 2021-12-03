6db0b62e45edfe050c79b1d05b05091e9f408db7
Micol Panetta <mi.panetta@reply.it>
2021-12-03 17:32:58 +0100

    refactoring + input 2

---
+++ src/adventofcode/Excercises.java
@@ -0,0 +1,48 @@
+package adventofcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Excercises {
+	
+	public static int exercise1a(List<Integer> numbers) {
+		int counter = 0;
+
+		for(int i = 1; i < numbers.size(); i++)
+			if(numbers.get(i) > numbers.get(i-1)) //{
+				counter++;
+//				System.out.println(numbers.get(i) + " INCREASED");
+//			} else if(numbers.get(i) < numbers.get(i-1)) {
+//				System.out.println(numbers.get(i) + " DECREASED");
+//			} else {
+//				System.out.println(numbers.get(i) + " EQUALS");
+//			}
+
+		return counter;
+	}
+
+	public static int exercise1b(List<Integer> numbers) {
+
+		List<Integer> sums = new ArrayList<>();
+
+		for(int i = 0; i < numbers.size(); i++) {
+
+			if(i == numbers.size() - 2)
+				break;
+
+			int y = numbers.get(i);
+			int j = numbers.get(i + 1);
+			int z = numbers.get(i + 2);
+
+			int sum = y + j + z;
+			//System.out.println(y + " + " + j + " + " + z + " = " + sum);
+
+			sums.add(sum);
+		}
+
+		return exercise1a(sums);
+
+	}
+
+
+}
--- src/adventofcode/Main.java
+++ src/adventofcode/Main.java
@@ -24,52 +24,11 @@ public class Main {
 			}
 			reader.close();
 
-			System.out.print(exercise1b(numbers));
+			System.out.print(Excercises.exercise1b(numbers));
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
-
-	private static int exercise1a(List<Integer> numbers) {
-		int counter = 0;
-
-		for(int i = 1; i < numbers.size(); i++)
-			if(numbers.get(i) > numbers.get(i-1)) //{
-				counter++;
-//				System.out.println(numbers.get(i) + " INCREASED");
-//			} else if(numbers.get(i) < numbers.get(i-1)) {
-//				System.out.println(numbers.get(i) + " DECREASED");
-//			} else {
-//				System.out.println(numbers.get(i) + " EQUALS");
-//			}
-
-		return counter;
-	}
-
-	private static int exercise1b(List<Integer> numbers) {
-
-		List<Integer> sums = new ArrayList<>();
-
-		for(int i = 0; i < numbers.size(); i++) {
-
-			if(i == numbers.size() - 2)
-				break;
-
-			int y = numbers.get(i);
-			int j = numbers.get(i + 1);
-			int z = numbers.get(i + 2);
-
-			int sum = y + j + z;
-			//System.out.println(y + " + " + j + " + " + z + " = " + sum);
-
-			sums.add(sum);
-		}
-
-		return exercise1a(sums);
-
-	}
-
-
 }
